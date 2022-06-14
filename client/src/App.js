baf1a5e3dddd3c48477c666d3b209095625171ff
Anthony Sutton <anthony.sutton95@gmail.com>
2022-06-14 07:27:14 -0700

    feat(delete): Able to delete clients via web app w/ caching

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,28 @@ import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from './components/Clients';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(_, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(_, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function App() {
